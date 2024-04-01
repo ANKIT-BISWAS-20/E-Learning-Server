5e57084abc496cbbd17f7d2c47a7c9ec8d3637fb
Ankit Biswas <biswasankit0004@gmail.com>
2024-04-01 12:07:46 +0530

    some changes in routes utils controllers and app

--- app.js
+++ app.js
@@ -12,4 +12,9 @@ app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 
 
+// Server Status
+import serverStatusRouter from "./src/routes/serverStatus.routes.js"
+app.use("/",serverStatusRouter);
+
+
 export { app }
