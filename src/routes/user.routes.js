8361b18788ac18a80babb164a40bc518d74cea29
Ankit Biswas <biswasankit0004@gmail.com>
2024-04-02 12:23:11 +0530

    Add getCurrentStudent and getCurrentMentor functions to user.controller.js

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerUser , loginUser, logoutUser, refreshAccessToken,updateUserAvatar,updateAccountDetails} from "../controllers/user.controller.js";
+import { registerUser , loginUser, logoutUser, refreshAccessToken,updateUserAvatar,updateAccountDetails,getCurrentStudent,getCurrentMentor} from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import { isMentor } from "../middlewares/isMentor.middleware.js";
