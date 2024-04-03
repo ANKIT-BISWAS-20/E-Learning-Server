af4fe704044ea6c3b9ea76de5d47fdc5bfa3774b
Ankit Biswas <biswasankit0004@gmail.com>
2024-04-03 16:58:21 +0530

    Add material model schema

---
+++ src/models/material.model.js
@@ -0,0 +1,35 @@
+import mongoose, {Schema} from "mongoose";
+
+const materialSchema = new Schema(
+    {
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+            required: true
+        },
+        name: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String, 
+            required: true,
+        },
+        file: {
+            type: String, 
+            required: true
+        },
+        class: {
+            type: Schema.Types.ObjectId,
+            ref: "Class",
+            required: true
+        },
+
+    }, 
+    {
+        timestamps: true
+    }
+)
+
+
+export const Material = mongoose.model("Material", materialSchema)
